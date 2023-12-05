cfd68b20778fded9033ba3cdd3a1f27d79276066
Ghost <jeremy.fernandez998@gmail.com>
2023-12-05 01:27:07 -0500

    Changed ports for Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,16 @@ dotenv.config();
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const { PORT = 3000 } = process.env;
+const PORT = process.env.PORT || 5173;
 
 export default defineConfig({
   plugins: [react({
-      jsxRuntime: 'classic' // Add this line
-    }
+    jsxRuntime: 'classic' // Add this line
+  }
   )],
   server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: PORT,
     proxy: {
       '/api': {
         target: `http://localhost:${PORT}`,
